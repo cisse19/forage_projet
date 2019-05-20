81d566baaf3e6630190d21412859bcb02df71c57
cheikh cisse <tidjanecisse19@gmail.com>
2019-05-20 18:31:23 +0000

    first commit

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,71 @@
+<div class="sidebar" data-color="purple" data-active-color="danger">
+    <!--
+      Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+  -->
+    <div class="logo">
+      <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+        <div class="logo-image-small">
+          <img src="{{asset('assets/img/senforage.jpg')}}">
+        </div>
+      </a>
+      <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+        SENforage
+        <!-- <div class="logo-image-big">
+          <img src="../assets/img/logo-big.png">
+        </div> -->
+      </a>
+    </div>
+    <div class="sidebar-wrapper">
+      <ul class="nav">
+        <li class="active ">
+          <a href="./dashboard.html">
+            <i class="nc-icon nc-bank"></i>
+            <p>TABLEAU DE BORD</p>
+          </a>
+        </li>
+        <li>
+          <a href="./user.html">
+            <i class="nc-icon nc-single-02"></i>
+            <p>GESTION UTILISATEURS</p>
+          </a>
+        </li>
+
+        <li>
+          <a href="./icons.html">
+            <i class="nc-icon nc-diamond"></i>
+            <p>GESTION COMPTEUR</p>
+          </a>
+        </li>
+        <li>
+          <a href="./tables.html">
+            <i class="nc-icon nc-tile-56"></i>
+            <p>LISTE DES UTILISATEURS</p>
+          </a>
+        </li>
+        <li>
+          <a href="./map.html">
+            <i class="nc-icon nc-pin-3"></i>
+            <p>CARTE</p>
+          </a>
+        </li>
+       <!--  <li>
+          <a href="./notifications.html">
+            <i class="nc-icon nc-bell-55"></i>
+            <p>Notifications</p>
+          </a>
+        </li>
+        <li>
+          <a href="./typography.html">
+            <i class="nc-icon nc-caps-small"></i>
+            <p>Typography</p>
+          </a>
+        </li>
+          <li class="active-pro">
+          <a href="./upgrade.html">
+            <i class="nc-icon nc-spaceship"></i>
+            <p>Upgrade to PRO</p>
+          </a>
+        </li>-->
+      </ul>
+    </div>
+  </div>
